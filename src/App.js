e6155914c9808cb2886c0001d64f7c840bfa11ca
forrestmorrison <forrestmorrison@gmail.com>
2022-12-12 12:47:40 -0600

    setup Redux store, no errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import {
+    connect,
+    Provider, 
+    useSelector,
+    useDispatch 
+} from 'react-redux'
+import { createStore } from '@reduxjs/toolkit';
+
+const countReducer = function(state = 0, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(countReducer);
+
+const mapStateToProps = state => {
+  return {
+    count: state
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    add: () => dispatch({type:"INCREMENT"}),
+    subtract: () => dispatch({type:"DECREMENT"})
+  }
+}
+const Component = ({count, add, subtract}) => 
+(<div>
+  <h1>Count = {count}</h1>
+  <button onClick={add}>Add</button>
+  <button onClick={subtract}>Subtract</button>
+</div>)
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Container />
+      </div>
+    </Provider>
   );
 }
 
